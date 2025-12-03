3fbcba87cc753e2a20b1903c6bb13fc4deeac20c
zz plant <kanavj@gmail.com>
2025-12-03 12:42:31 -0500

    Add MDX homepage with typed content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [react(), icon(), sitemap()],
+  integrations: [react(), icon(), mdx(), sitemap()],
 });
