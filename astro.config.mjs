c0d497f9d430deb6358178094f37e2b8839e90b3
zz plant <kanavj@gmail.com>
2025-12-23 17:19:03 -0600

    Improve build performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,5 +36,11 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
+    build: {
+      cssMinify: 'lightningcss',
+    },
+    ssr: {
+      external: ['node:crypto', 'node:fs/promises', 'node:path', 'node:url'],
+    },
   },
 });
